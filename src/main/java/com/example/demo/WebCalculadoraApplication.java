936f8d0c117a41147bbf99f2972972c39b0bd3cd
Jonhy <Jonhy@JONHY>
2021-05-24 21:46:13 -0500

    creacion de ficheros

---
+++ src/main/java/com/example/demo/WebCalculadoraApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCalculadoraApplication.class, args);
+	}
+
+}
+
+
